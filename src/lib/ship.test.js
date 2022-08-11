db7595cf9b1ff3ed0bd3dc595770da34177b4066
crespire <li.simmon@gmail.com>
2022-08-11 12:17:26 -0400

    Update ship tests to reflect changes in ship

--- src/lib/ship.test.js
+++ src/lib/ship.test.js
@@ -2,23 +2,29 @@ import { ShipBuilder } from './ship';
 
 
 describe('when creating a ship', () => {
-  test('creates a ship with the right anchor property', () => {
-    const ship = ShipBuilder('c2', 4);
-  
-    expect(ship.getAnchor()).toBe('c2');
+  describe('when creating a horizontally oriented ship', () => {
+    test('a ship is created correctly', () => {
+      const ship = ShipBuilder('c2', 3);
+      expect(ship.getAnchor()).toBe('c2');
+      expect(ship.getLength()).toBe(3);
+    });
   });
+
+  describe('when creating a vertically oriented ship', () => {
+    test('a ship is created correctly', () => {
+      const ship = ShipBuilder('c2', 5, false);
+      expect(ship.getAnchor()).toBe('c2');
+      expect(ship.getLength()).toBe(5);
+    })
+  })
 })
 
-describe('when a ship is created', () => {
+describe('after a ship is created', () => {
   const ship = ShipBuilder('c2', 2);
   
   test('updates a ship when a hit is registered', () => {
-    ship.setHit(1);
-    expect(ship.getAllCells()).toStrictEqual([false, true]);
-  });
-
-  test('correctly returns true when asked about cell index 1', () => {
-    expect(ship.getCell(1)).toStrictEqual(true);
+    ship.receiveHit('c2');
+    expect(ship.showHits()).toStrictEqual([true, false]);
   });
 
   test('correctly reports a ship still in play after a non-fatal hit', () => {
@@ -26,24 +32,8 @@ describe('when a ship is created', () => {
   });
 
   test('correctly reports a ship is sunk', () => {
-    ship.setHit(0);
-    expect(ship.getCell(0)).toStrictEqual(true);
+    ship.receiveHit('d2');
+    expect(ship.showHits()).toStrictEqual([true, true]);
     expect(ship.isSunk()).toStrictEqual(true);
   });
-})
-
-describe('when dealing with out of bounds values for #setHit', () => {
-  const ship = ShipBuilder('c2', 1);
-
-  test('handles out of bounds above limit gracefully', () => {
-    expect(() => {
-      ship.setHit(6);
-    }).toThrow('Not in bounds');
-  });
-
-  test('handles out of bounds below limit gracefully', () => {
-    expect(() => {
-      ship.setHit(-1);
-    }).toThrow('Not in bounds');
-  });
-})
+});

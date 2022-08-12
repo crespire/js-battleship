cdec74e164314b60324e1fc47608a917efd56aa0
crespire <li.simmon@gmail.com>
2022-08-12 14:41:59 -0400

    Finish gameboard factory

--- src/lib/gameboard.js
+++ src/lib/gameboard.js
@@ -6,6 +6,8 @@
 
 const BoardBuilder = (owner) => {
   let boardData = Array.from(Array(10).fill(0), () => new Array(10).fill(0));
+  let ships = [];
+  let attackHistory = [];
   
   const _getData = () => {
     return boardData;
@@ -13,34 +15,95 @@ const BoardBuilder = (owner) => {
 
   const _logState = () => {
     console.log(boardData);
+    console.log(ships);
   }
 
+  /**
+   * Resets the board.
+   * @returns {true} after completion.
+   * @access public
+   */
   const clearBoard = () => {
-    console.log('Re-initializing board.');
     boardData = Array.from(Array(10).fill(0), () => new Array(10).fill(0));
+    return true;
   }
 
+  /**
+   * Takes a location and updates the board data based on what was there.
+   * @param {string} location - in format 'a1'
+   * @returns {true} when complete
+   * @access public
+   */
   const receiveAttack = (location) => {
-    let inBounds = validCoordinate(location);
-    if (!inBounds) throw new Error('Coordinates out of bounds.');
+    validateCoordinate(location);
+    if (attackHistory.includes(location)) throw new Error('Already attacked:', location);
 
+    attackHistory.push(location);
     let data = getCell(location);
     if ( data == 0 ) {
-      console.log('Miss!');
-      data = 1;
-    } else if ( data == 1 ) {
-      console.log('Already marked miss.');
+      console.log('Miss:', location);
+      setCell(location, 1)
     } else if ( typeof data == 'function') {
-      // we found a ship, but I have to figure out how to get which part of the ship
+      console.log('Hit:', location)
+      data(location);
+      setCell(location, 'x');
+    } else {
+      throw new Error('Something went wrong, data is not expected', data);
     }
+
+    return true;
   };
 
-  const placeShip = (origin, ship) => {
+  /**
+   * Returns the amount of misses recorded on the board.
+   * @returns {num}
+   * @access public
+   */
+  const getMisses = () => {
+    return boardData.flat().filter(element => element == 1).length
+  }
+
+  /**
+   * Returns the amount of hits recorded on the board.
+   * @returns {num}
+   * @access public
+   */
+  const getHits = () => {
+    return boardData.flat().filter(element => element == 'x').length
+  }
+
+  /**
+   * Calculates whether all ships on the board are sunk.
+   * @returns {bool}
+   * @access public
+   */
+  const allSunk = () => {
+    return ships.every((ship) => { return ship.isSunk() });
+  }
+  
+  /**
+   * Returns an array of all ship objects
+   * @returns {array}
+   * @access public
+   */
+  const allShips = () => {
+    return ships;
+  }
+
+  /**
+   * Places a ship on the board if the ship is in bounds and does not
+   * intersect another ship.
+   * @param {Ship} ship 
+   * @returns {true} on success
+   * @access public
+   */
+  const placeShip = (ship) => {
+    let origin = ship.getAnchor();
     let horizontal = ship.isHorizontal();
     let shipLength = ship.getLength();
     let cells = ship.getCells();
-    let validPlacement = cells.every((coordinate) => validCoordinate(coordinate));
-    if (!validPlacement) return false;
+    cells.every((coordinate) => validateCoordinate(coordinate));
+    cells.every((coordinate) => validateCellEmpty(coordinate));
 
     if (horizontal) {
       for (let i = 0; i < shipLength; i++) {
@@ -52,17 +115,18 @@ const BoardBuilder = (owner) => {
       }
     }
 
+    ships.push(ship);
     return true;
   };
-  
+
   /**
    * A method to check if the coordinate is in bounds.
    * @param {string} coord - Coordinate in format 'a1'
    * @throws Will throw error if coordinate is not in bounds.
-   * @returns {true}
+   * @returns {true} provided guard conditions pass.
    * @access public
    */
-  const validCoordinate = (coord) => {
+  const validateCoordinate = (coord) => {
     let [col, row] = [...coord];
     const colValidation = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
     const rowValidation = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
@@ -71,12 +135,28 @@ const BoardBuilder = (owner) => {
 
     return true;
   }
+
+  /**
+   * A method to check if the coordinate is empty
+   * @param {string} coord - Coordinate in format 'a1'
+   * @throws Will throw error if coordinate contains a function.
+   * @returns {true} provided guard conditions pass.
+   * @access private
+   */
+  const validateCellEmpty = (coord) => {
+    let target = getCell(coord);
+    if (typeof target == 'function') throw new Error('Already occupied at', coord);
+
+    return true;
+  }
+
   /**
    * A method to set the value of a given cell
    * @param {string} location - A1 through J10
    * @param {*} value - Value for cell
    * @param {int} colOffset - Number of columns to offset
    * @param {int} rowOffset - Number of rows to offset
+   * @returns {true} if update was successful
    * @access public
    */
   const setCell = (location, value, colOffset = 0, rowOffset = 0) => {
@@ -88,6 +168,7 @@ const BoardBuilder = (owner) => {
     if (y > 9 || y < 0) throw new Error(`Offset out of bounds, y is ${y}`);
 
     boardData[x][y] = value;
+    return true;
   };
 
   /**
@@ -138,8 +219,12 @@ const BoardBuilder = (owner) => {
     receiveAttack,
     getCell,
     setCell,
+    getMisses,
+    getHits,
     placeShip,
     clearBoard,
+    allSunk,
+    allShips,
     _getData,
     _logState,
   };

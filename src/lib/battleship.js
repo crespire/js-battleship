843ff4ab08e8d87c5babe382e20549bff38e290f
crespire <li.simmon@gmail.com>
2022-08-23 15:48:11 -0400

    Move randomCell to board, and remove playRound()

--- src/lib/battleship.js
+++ src/lib/battleship.js
@@ -15,22 +15,6 @@ const Battleship = () => {
     let board = BoardBuilder(owner);
 
     // Generate ships randomly and place them.
-    const randomCell = () => {
-      let cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-      let rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-      let choices = [];
-      for(let i = 0; i < 10; i++) {
-        for(let j = 0; j < 10; j++) {
-          choices.push([cols[i], rows[j]].join(''));
-        };
-      };
-  
-      let choiceIndex = Math.floor(Math.random() * choices.length);
-      let result = choices[choiceIndex];
-  
-      return result;
-    }
-
     let shipLength = 5;
     let ship;
     let anchor;
@@ -46,7 +30,7 @@ const Battleship = () => {
      * 83 misses for full
      */
     while(!success) { // Add one 3 length ship.
-      anchor = randomCell();
+      anchor = board.randomCell();
       horizontal = Math.random() < 0.5;
       ship = ShipBuilder(anchor, 3, horizontal);
       success = board.placeShip(ship);
@@ -54,7 +38,7 @@ const Battleship = () => {
 
     success = false;
     while(shipLength > 1) { // Add the rest.
-      anchor = randomCell();
+      anchor = board.randomCell();
       horizontal = Math.random() < 0.5;
       ship = ShipBuilder(anchor, shipLength, horizontal);
       success = board.placeShip(ship);
@@ -62,12 +46,6 @@ const Battleship = () => {
     }
 
     return board;
-  }
-
-  const playRound = () => {
-    // Player goes
-    // Computer goes
-    return true;
   }
 
   const renderBoard = (board, showShips = true) => {
@@ -108,7 +86,7 @@ const Battleship = () => {
             cell.classList.add('cell--hit');
             break;
           default:
-            showShips ? cell.classList.add('cell--ship') : cell.classList.add('cell--empty');
+            (showShips) ? cell.classList.add('cell--ship') : cell.classList.add('cell--empty');
             break;
         }
         parentDiv.appendChild(cell);
@@ -125,6 +103,7 @@ const Battleship = () => {
     }
 
     let playerBoard = setupBoard(player);
+    console.log(playerBoard._logState());
     let computerBoard = setupBoard(computer);
     const root = document.getElementById('root');
     console.log('Boards set up.');
